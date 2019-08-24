7ecd47b6270249f74dbd8a8534d7b8dca93eec1e
Ighour <ti.celio.rodrigues@gmail.com>
2019-08-23 21:18:06 -0300

    Refactored INTRO to compact.

--- src/app/scenes/intro.js
+++ src/app/scenes/intro.js
@@ -1,32 +1,41 @@
 const { GAME } = require('../game');
 
 /** Variables */
-let startButton, keyboardPosition, keySize, capsLock;
-let creating, name, keyboard, nameMax, nameRules;
+let startButton, keyboardPosition, keySize, capsLock, creating, name, keyboard, nameMax, nameRules;
 
 /** Events */
 let clickStart = () => {
-    //Create
-    if(!creating)
-        creating = true;
-    //Start
-    else if(name.length > 0)
-        beginGame();
+    creating = true;
+
+    if(name.length > 0){
+        GAME.p.n = name;
+        GAME.n(true);
+    }
 };
 
 let clickKeyboard = (event, x, y) => {
-    if(creating)
-        pressKey(x, y);
+    if(creating){
+        let keyIndex = Math.floor((x - keyboardPosition.x) / keySize.w) + Math.floor((y - keyboardPosition.y) / keySize.h) * 10;
+
+        if(keyIndex === 29)
+            capsLock = !capsLock;
+        else if(keyIndex === 38){
+            if(name.length > 0 && name.length < nameMax)
+                name += " ";
+        }
+        else if(keyIndex === 39)
+            name = name.slice(0, name.length - 1);
+        else if(name.length < nameMax)
+            name += capsLock ? keyboard[keyIndex] : keyboard[keyIndex].toLowerCase();
+    }
 };
 
 let keyDown = (event) => {
     if(!creating)
         return;
         
-    let key = event.key;
-
-    if(validKeyForName(key) && name.length < nameMax)
-        name += key;
+    if(name.length < nameMax && event.key.length === 1 && nameRules.test(event.key))
+        name += event.key;
     else if(event.keyCode === 8) //Backspace
         name = name.slice(0, name.length - 1);
     else if(event.keyCode === 20) //Caps Lock
@@ -34,100 +43,59 @@ let keyDown = (event) => {
 };
 
 /** Helper Functions */
-let validKeyForName = key => {
-    if(name.length === 0 && key === " ")
-        return false;
-
-    return key.length === 1 && nameRules.test(key);
-};
 
 /** Logic */
 // let logic = () => {};
 
-let beginGame = () => {
-    GAME.p.n = name;
-    GAME.n(true);
-};
-
-let pressKey = (x, y) => {
-    let target = {
-        x: Math.floor((x - keyboardPosition.x) / keySize.w),
-        y: Math.floor((y - keyboardPosition.y) / keySize.h),
-    };
-
-    let keyIndex = target.x + target.y * 10;
-
-    if(keyIndex === 29)
-        capsLock = !capsLock;
-    else if(keyIndex === 38){
-        if(name.length > 0 && name.length < nameMax)
-            name += " ";
-    }
-    else if(keyIndex === 39)
-        name = name.slice(0, name.length - 1);
-    else if(name.length < nameMax){
-        name += capsLock ? keyboard[keyIndex] : keyboard[keyIndex].toLowerCase()
-    }
-};
-
 /** Draw */
 let draw = () => {
-    if(creating === false)
-        drawNew();
-    else
-        drawCreate();  
-};
+    let x = GAME.c.w / 2, y = GAME.c.h / 2;
 
-let drawNew = () => {
-    //Title
-    GAME.d.ft("Back To #", GAME.c.w / 2, GAME.c.h / 5, {f: 100});
+    if(creating === false){
+        //Title
+        GAME.d.ft("Back To #", x, y / 4, {f: 100});
 
-    //Brief
-    let texts = [
-        "Welcome to a journey back through time",
-        "where some games were corrupted.",
-        "Now only a true gamer",
-        "can proceed!"
-    ];
-    GAME.d.ftb(texts, GAME.c.w / 2, GAME.c.h * 2 / 5, 70);
+        //Brief
+        let texts = [
+            "Welcome to a journey back through time",
+            "where some games were corrupted.",
+            "Now only a true gamer",
+            "can proceed!"
+        ];
+        GAME.d.ftb(texts, x, y / 1.3, 70);
 
-    //Start Button
-    GAME.d.db(startButton, "Start Game");
-};
-
-let drawCreate = () => {
-    //Call
-    let texts = [
-        "May I know your name,",
-        "Traveler?"
-    ];
-    GAME.d.ftb(texts, GAME.c.w / 2, GAME.c.h / 10, 70);
-
-    //Input
-    GAME.d.fr(GAME.c.w/ 3, GAME.c.h * 3 / 10, GAME.c.w / 3, 46);
-    GAME.d.ft(name, GAME.c.w / 2, GAME.c.h * 3 / 10 + 23, {fs: "#222222"});
-
-    //Keyboard
-    drawKeyboard();
-
-    //Send Button
-    if(name.length > 0)
-        GAME.d.db(startButton, "Time Travel");
-};
-
-let drawKeyboard = () => {
-    GAME.d.sr(keyboardPosition.x, keyboardPosition.y, keyboardPosition.w, keyboardPosition.h, {ss: "#555555"});
-
-    for(let i = 0; i < keyboard.length; i++){
-        let x = keyboardPosition.x + keySize.w / 2 + keySize.w * (i % 10);
-        let y = keyboardPosition.y + keySize.h / 2 + keySize.h * Math.floor(i / 10);
-        let key = keyboard[i];
-
-        if(!capsLock && i > 9 && !["Caps", "_", "Space", "Del"].includes(key))
-            key = key.toLowerCase();
-
-        GAME.d.ft(key, x, y, {f: 25});    
+        //Start Button
+        GAME.d.db(startButton, "Start Game");
     }
+
+    else{
+        //Call
+        let texts = [
+            "May I know your name,",
+            "Traveler?"
+        ];
+        GAME.d.ftb(texts, x, y / 4, 70);
+
+        //Input
+        GAME.d.fr(x - 160, y / 1.8, 320, 46);
+        GAME.d.ft(name, x, y / 1.8 + 23, {fs: "#222222"});
+
+        //Keyboard
+        GAME.d.sr(keyboardPosition.x, keyboardPosition.y, keyboardPosition.w, keyboardPosition.h, {ss: "#555555"});
+
+        for(let i = 0; i < keyboard.length; i++){
+            let key = keyboard[i];
+    
+            if(!capsLock && i > 9 && !["Caps", "_", "Space", "Del"].includes(key))
+                key = key.toLowerCase();
+    
+            GAME.d.ft(key, keyboardPosition.x + keySize.w / 2 + keySize.w * (i % 10), keyboardPosition.y + keySize.h / 2 + keySize.h * Math.floor(i / 10), {f: 25});    
+        }
+
+        //Send Button
+        if(name.length > 0)
+            GAME.d.db(startButton, "Time Travel");
+    } 
 };
 
 /** Lifecycle */
