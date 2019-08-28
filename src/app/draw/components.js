3aebc973c8e355574db9a82c86c3b47df4852d28
Ighour <ti.celio.rodrigues@gmail.com>
2019-08-27 22:32:20 -0300

    Fixed life UI hp less then 0.

--- src/app/draw/components.js
+++ src/app/draw/components.js
@@ -34,6 +34,7 @@ let rad = deg => Math.PI / 180 * deg;
 let percentToRad = percent => rad(270) + rad(360 * percent / 100);
 
 export const drawLifeCircle = (draw, x, y, radius, life, lw = 5) => {
+    if(life < 0) life = 0;
     draw.sc(x, y, radius, rad(270), percentToRad(100), {ss: "#EEEEEE", lw});
     draw.sc(x, y, radius, rad(270), percentToRad(life), {ss: "rgba(255,0,0,0.8)", lw});
     draw.fc(x, y, radius - lw + 3, 0, 2*Math.PI, {fs: "#15AAAA", lw});
