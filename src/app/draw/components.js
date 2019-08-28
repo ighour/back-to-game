df33cccd05db8ac089f7292a934cfa0990adc4ae
Ighour <ti.celio.rodrigues@gmail.com>
2019-08-27 22:28:36 -0300

    Added life number to panel

--- src/app/draw/components.js
+++ src/app/draw/components.js
@@ -37,6 +37,7 @@ export const drawLifeCircle = (draw, x, y, radius, life, lw = 5) => {
     draw.sc(x, y, radius, rad(270), percentToRad(100), {ss: "#EEEEEE", lw});
     draw.sc(x, y, radius, rad(270), percentToRad(life), {ss: "rgba(255,0,0,0.8)", lw});
     draw.fc(x, y, radius - lw + 3, 0, 2*Math.PI, {fs: "#15AAAA", lw});
+    draw.ft(Math.ceil(life), x, y, {fs: "rgba(255,0,0,0.5)", f: 30});
 };
 
 /** Arrow Pointer */
